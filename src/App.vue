295dbd0d33f90587f61aa146ab367b63f0cfd7b4
Ivan <kimagure44@yahoo.es>
2021-12-01 19:07:49 +0100

    feature: data import and navbar

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,23 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-btn
+          href="https://www.nnergix.com/"
+          target="_blank"
+          text
+          plain
+        >
+          <v-img
+            alt="NNergix Logo"
+            class="shrink mr-2"
+            contain
+            :src="logo"
+            transition="scale-transition"
+            width="90"
+          />
+        </v-btn>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
     </v-app-bar>
-
     <v-main>
       <router-view/>
     </v-main>
@@ -44,12 +30,13 @@
 </template>
 
 <script>
+import logo from '@/assets/logo.webp';
 
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    logo,
   }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <hello-world />
+  <h1>Home</h1>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   name: 'Home',
 
-  components: {
-    HelloWorld,
-  },
 };
 </script>
